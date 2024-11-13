75dd3b42bfe6cd342836ccf6dc36447d9e88be15
Chinh <chinhcaocu@gmail.com>
2024-11-13 20:24:49 +0700

    Static website (not finalized wil add animations later)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
-  title: "VECTR's Teacher Day",
+  title: "Teacher Day Card",
   description: "Owned by VECTR",
 };
+
+const BaskervvilleSC = localFont({
+  src: "../fonts/Baskervville_SC/BaskervvilleSC-Regular.ttf",
+  variable: "--font-basker",
+});
+const PlayFairDisplay = localFont({
+  src: "../fonts/Playfair_Display/PlayfairDisplay-VariableFont_wght.ttf",
+  variable: "--font-playfair",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${BaskervvilleSC.variable} ${PlayFairDisplay.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,42 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ENGLISH_PROMPT, VIETNAMESE_PROMPT } from "@/constants/promp";
 
 export default function Home() {
-  const [card, setCard] = useState(Math.floor(Math.random() * 6) + 1);
+  const number_of_cards = 5;
+  const number_of_prompts = ENGLISH_PROMPT.length;
+  const [card, setCard] = useState(1);
+  const [prompt, setPrompt] = useState(1);
 
-  const randomNumber = () => {
-    if (card < 6) {
+  useEffect(() => {
+    setCard(Math.floor(Math.random() * number_of_cards) + 1);
+    setPrompt(Math.floor(Math.random() * number_of_prompts) + 1);
+  }, [number_of_prompts]);
+
+  const nextElement = () => {
+    if (card < number_of_cards) {
       setCard(card + 1);
     } else {
       setCard(1);
     }
+    if (prompt < number_of_prompts) {
+      setPrompt(card + 1);
+    } else {
+      setPrompt(1);
+    }
+  };
+  const adjustCSS = (property: "width" | "top" | "left") => {
+    const cardType = card === 2 || card === 4 || card === 5;
+
+    const styles = {
+      width: cardType ? "58%" : "50%",
+      top: cardType ? "30%" : "18%",
+      left: cardType ? "31%" : "25%",
+    };
+
+    return styles[property];
   };
 
   return (
@@ -21,23 +46,47 @@ export default function Home() {
       }}
       className={styles.page}
     >
-      <div className="container">
-        <Image
-          src={`/Card${card}/letter.png`}
-          height={736}
-          width={525}
-          alt="letter"
-          className={styles.letter}
-        />
-        <Image
-          src={`/Card${card}/envelope.png`}
-          height={512}
-          width={365}
-          alt="letter"
-          className={styles.envelope}
-        />
+      <div className={styles.container}>
+        <div className={styles.letter_container}>
+          <Image
+            src={`/Card${card}/letter.png`}
+            height={707}
+            width={504}
+            alt="letter"
+            className={styles.letter}
+          />
+          <div
+            className={styles.text_container}
+            style={{
+              width: adjustCSS("width"),
+              top: adjustCSS("top"),
+              left: adjustCSS("left"),
+            }}
+          >
+            <p className={styles.vietnamese}>{VIETNAMESE_PROMPT[prompt]}</p>
+            <p className={styles.english}>{ENGLISH_PROMPT[prompt]}</p>
+          </div>
+        </div>
+        <div className={styles.envelope_container}>
+          <Image
+            src={`/Card${card}/envelope.png`}
+            height={614}
+            width={438}
+            alt="letter"
+            className={styles.envelope}
+          />
+          <button onClick={nextElement} className={styles.switch_button}>
+            Đổi/Change{" "}
+            <Image
+              src={`/vectr.png`}
+              height={20}
+              width={20}
+              alt="VECTR LOGO"
+              className={styles.logo}
+            />
+          </button>
+        </div>
       </div>
-      <button onClick={randomNumber}>Skibidi</button>
     </div>
   );
 }
